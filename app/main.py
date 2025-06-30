2b3037e9167d17407cfc1ddc6f3cd16342afd882
Valentina Chicuasuque Rodriguez <141190350+ValentinaChicua@users.noreply.github.com>
2025-06-30 18:12:32 -0500

    creacion de metricas (#6)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+from app.core.metrics import REQUEST_COUNT, RESPONSE_TIME, ERROR_COUNT
+
+import time
 
 # Importar routers
 from app.api import studentGrades, studentResponses
@@ -23,6 +28,40 @@ app.add_middleware(
 # Incluir routers
 app.include_router(studentGrades.router, prefix="/grades", tags=["Student Grades"])
 app.include_router(studentResponses.router, prefix="/responses", tags=["Student Responses"])
+
+# Middleware para registrar métricas
+@app.middleware("http")
+async def record_metrics(request, call_next):
+    method = request.method
+    endpoint = request.url.path
+
+    # Registrar la hora de inicio para medir el tiempo de respuesta
+    start_time = time.time()
+
+    # Continuar con la solicitud
+    response = await call_next(request)
+
+    # Medir tiempo de respuesta
+    duration = time.time() - start_time
+    RESPONSE_TIME.labels(method=method, endpoint=endpoint).observe(duration)
+
+    # Incrementar contador de peticiones
+    REQUEST_COUNT.labels(method=method, endpoint=endpoint).inc()
+
+    # Si hay un error (código de estado >= 400), aumentar el contador de errores
+    if response.status_code >= 400:
+        ERROR_COUNT.labels(method=method, endpoint=endpoint).inc()
+
+    return response
+
+# Ruta para exponer las métricas en formato Prometheus
+@app.get("/metrics")
+async def metrics():
+    data = generate_latest()
+    return Response(content=data, media_type=CONTENT_TYPE_LATEST)
+
+
+
 
 @app.get("/")
 async def root():
